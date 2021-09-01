23fbedada2cf02e00b7c56b97c90dd3a8fc2933c
Mahmoud Kamal <61159931+Mahmoud-kamal12@users.noreply.github.com>
2021-09-02 01:17:08 +0200

    Update google-config.php

--- controllers/google-config.php
+++ controllers/google-config.php
@@ -2,7 +2,7 @@
 
 $clientID = '150184604237-88i8ulueute38k9igqr44r1lsn66rmkb.apps.googleusercontent.com';
 $clientSecret = 'Ayk1QgTHvJQDVhbZipjx6OUv';
-$redirect = 'http://localhost:8088/Colage%20Project/E-Commerce/controllers/google.php';
+$redirect = 'https://ecommercemahmoud.herokuapp.com/controllers/google.php';
 
 $client = new Google_Client();
 $client->setClientId($clientID);
